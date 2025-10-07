3a4b324235275eed68aaddbbe5bde435a18cc20e
aisla011 <aisla011@gold.ac.uk>
2025-10-07 15:40:47 +0100

    html included

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ var http = require("http");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!!!");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+        <!doctype html>
+            <html>
+                <head>
+                    <title>My Page</title>
+                    <h1>My Page!</h1>
+                </head>
+                <body>
+                    <p>Hello, My name is Anjuman.</p>
+                </body> 
+            </html>`)
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 });
